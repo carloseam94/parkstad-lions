355ae312bfb408fc85248f877229609c7fb0c79c
Carlos <carloseam94@gmail.com>
2025-07-04 14:47:20 +0200

    feat: decouple training cards. badegs blue

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,12 @@ export enum Locale {
   EN = 'en',
   NL = 'nl',
 }
+
+export type TrainingCard = {
+  name: string;
+  ageGroup: string;
+  image: string;
+  trainingDays: string;
+  description: string[];
+  coaches: { name: string; phone: string;}[];
+}
