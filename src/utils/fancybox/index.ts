f481165c1346ac8db2e77a385e950a05fd0f09f3
Carlos <carloseam94@gmail.com>
2025-07-04 15:24:57 +0200

    feat: add client-only for js libraries on prod

--- src/utils/fancybox/index.ts
+++ src/utils/fancybox/index.ts
@@ -1,6 +1,8 @@
 import { Fancybox } from '@fancyapps/ui';
 import '../../../node_modules/@fancyapps/ui/dist/fancybox/fancybox.css';
 
+console.log('Fancybox initialized');
+
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
 }); 
