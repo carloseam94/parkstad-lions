d8e73b88ec3c654fd566331dc915465c21844773
carloseam94 <carloseam94@gmail.com>
2025-07-05 21:21:55 +0200

    feat: remove embla and make carousel with fancybox

--- src/utils/fancybox/index.ts
+++ src/utils/fancybox/index.ts
@@ -1,6 +1,26 @@
-import { Fancybox } from '@fancyapps/ui';
-import '../../../node_modules/@fancyapps/ui/dist/fancybox/fancybox.css';
+import { Fancybox } from "@fancyapps/ui";
+import { Carousel } from "@fancyapps/ui/dist/carousel/";
+import { Autoplay } from "@fancyapps/ui/dist/carousel/carousel.autoplay.js";
+import { Thumbs } from "@fancyapps/ui/dist/carousel/carousel.thumbs.js";
+import { Arrows } from "@fancyapps/ui/dist/carousel/carousel.arrows.js";
+import "../../../node_modules/@fancyapps/ui/dist/carousel/carousel.arrows.css";
+import "../../../node_modules/@fancyapps/ui/dist/carousel/carousel.thumbs.css";
+import "../../../node_modules/@fancyapps/ui/dist/carousel/carousel.css";
+import "../../../node_modules/@fancyapps/ui/dist/fancybox/fancybox.css";
+import "@fancyapps/ui/dist/carousel/carousel.autoplay.css";
+
+const container = document.getElementById("gallery-carousel");
+const options = {
+  Autoplay: {
+    timeout: 5000,
+  },
+  Thumbs: {
+    type: "modern",
+  },
+};
 
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
-}); 
+});
+
+Carousel(container, options, { Autoplay ,Thumbs, Arrows }).init();
