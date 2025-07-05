edfd3390cd77c68c2f39696ef11af8391c6ed889
carloseam94 <carloseam94@gmail.com>
2025-07-05 14:58:23 +0200

    feat: banner i18n

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,10 +9,20 @@ export const defaultLocale = Locale.NL;
 
 export const ui = {
   en: {
+    "joinus": "Join us",
     "banner.legacy": "Join the Legacy!",
+    "banner.bePartOf": "Be part of our",
+    "banner.baseball": "Baseball",
+    "banner.team": "team",
+    "banner.experience": "Experience the thrill of baseball, camaraderie, and community spirit. Your journey starts here."
    
   },
   nl: {
+    "joinus": "sluit je bij ons aan",
     "banner.legacy": "Sluit je aan bij de Erfenis!",
+    "banner.bePartOf": "Maak deel uit van onze",
+    "banner.baseball": "Honkbal",
+    "banner.team": "team",
+    "banner.experience": "Ervaar de spanning van honkbal, kameraadschap en gemeenschapszin. Je reis begint hier."
   },
 } as const;
