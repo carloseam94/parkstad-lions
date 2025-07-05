2e080f7ab729864bd095efe654833ce4f1d0a79d
carloseam94 <carloseam94@gmail.com>
2025-07-05 15:26:58 +0200

    feat: join us i18n and decoupling

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -22,7 +22,10 @@ export const ui = {
     "banner.experience": "Experience the thrill of baseball, camaraderie, and community spirit. Your journey starts here.",
     "aboutus.title": "Who are we?",
     "aboutus.description": "Parkstad Lions is the baseball and softball club for Parkstad and the surrounding Dutch and German region. Everybody, young and old, boys and girls with all nationalities is welcome to join practice on our fields in Heerlen.",
-   
+    "joinus.card1.title": "Discover the Joy of Baseball!",
+    "joinus.card1.description": "Baseball is a dynamic, team-oriented sport where children develop skills in catching, throwing, and hitting—all while enjoying exciting matches against other teams across Limburg. Our players form a welcoming and supportive community where every child feels included and valued.",
+    "joinus.card2.title": "First Month Free – We’ve Got You Covered!",
+    "joinus.card2.description": "Start with a free month of training to explore everything we offer—no strings attached. We believe every child should have the opportunity to participate, so we'll always do what we can to support families with practical help, whether that’s assistance with costs or lending equipment like uniforms.",
   },
   nl: {
     "aboutus": "Over ons",
@@ -38,5 +41,9 @@ export const ui = {
     "banner.experience": "Ervaar de spanning van honkbal, kameraadschap en gemeenschapszin. Je reis begint hier.",
     "aboutus.title": "Wie zijn wij?",
     "aboutus.description": "Parkstad Lions is de honkbal en softbalvereniging van Parkstad en de omliggende Nederlandse en Duitse regio. Iedereen, jong en oud, jongens en meisjes van alle nationaliteiten, is welkom om te trainen op onze velden in Heerlen.",
+    "joinus.card1.title": "Ontdek de vreugde van honkbal!",
+    "joinus.card1.description": "Honkbal is een dynamische, teamgerichte sport waarbij kinderen vaardigheden ontwikkelen in vangen, gooien en slaan – en dat allemaal terwijl ze spannende wedstrijden spelen tegen andere teams in heel Limburg. Onze spelers vormen een gastvrije en ondersteunende gemeenschap waar elk kind zich thuis voelt en gewaardeerd wordt.",
+    "joinus.card2.title": "Eerste maand gratis – Wij staan ​​voor u klaar!",
+    "joinus.card2.description": "Begin met een gratis maand training om alles te ontdekken wat we te bieden hebben, zonder verplichtingen. Wij vinden dat elk kind de kans moet krijgen om mee te doen, dus we doen er alles aan om gezinnen te ondersteunen met praktische hulp, of het nu gaat om hulp bij de kosten of het uitlenen van spullen zoals uniformen.",
   },
 } as const;
