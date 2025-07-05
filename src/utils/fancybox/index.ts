94418617a4756eed14ec908752eea19939932886
carloseam94 <carloseam94@gmail.com>
2025-07-05 19:04:28 +0200

    refactor: remove logs

--- src/utils/fancybox/index.ts
+++ src/utils/fancybox/index.ts
@@ -1,8 +1,6 @@
 import { Fancybox } from '@fancyapps/ui';
 import '../../../node_modules/@fancyapps/ui/dist/fancybox/fancybox.css';
 
-console.log('Fancybox initialized');
-
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
 }); 
